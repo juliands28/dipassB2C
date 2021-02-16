0d270711934443795bfa91e0a09729c91179243b
juliands28 <dhinihernandez@gmail.com>
2021-02-16 10:08:33 +0700

    update ke hosting

--- public/js/page-loading.js
+++ public/js/page-loading.js
@@ -9,7 +9,7 @@ if (typeof Pace != "undefined") {
     var soapPageLoadingContent = false;
     //document.write('<img alt="" src="images/logo2.png" style="display: none;">');
     var logoImg = new Image();
-    logoImg.src = "/images/logo.png";
+    logoImg.src = "{{ asset('/images/logo.png') }}";
     var soapPageLoadingProgressInterval = setInterval(function() {
         try {
             if (document.body.className.indexOf("pace-done") != -1) {
